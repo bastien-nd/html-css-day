c9c4db3255986bf4fe073e50ab7a2fc4ae0acdee
fuzoh <info@bastiennicoud.ch>
2022-10-26 12:33:51 +0200

    Display content of an input in the title

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 console.log('bonjour depuis le navigateur')
 
-let toto = document.getElementById('sidebar')
+let toto = document.getElementById('email-contact-form')
 
-toto.addEventListener("click", function () {
+toto.addEventListener("input", function (e) {
 
   let articles = document.getElementsByTagName('article')
 
   for (const article of articles) {
     article.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16)
   }
+
+  document.getElementsByTagName('h1')[0].innerHTML = e.target.value
+
 })
