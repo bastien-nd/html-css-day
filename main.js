e6c1b721163efd9e567612736b36b0bf076fda9a
fuzoh <info@bastiennicoud.ch>
2022-10-26 11:20:25 +0200

    Add random colors to the articles elements

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('bonjour depuis le navigateur')
+
+let toto = document.getElementById('sidebar')
+
+toto.addEventListener("click", function () {
+
+  let articles = document.getElementsByTagName('article')
+
+  for (const article of articles) {
+    article.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+  }
+})
