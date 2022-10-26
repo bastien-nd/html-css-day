bc3742f90eafbfc7681300b413be4257abe92f84
fuzoh <info@bastiennicoud.ch>
2022-10-26 13:50:09 +0200

    Add function to retrieve grades

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ toto.addEventListener("input", function (e) {
   document.getElementsByTagName('h1')[0].innerHTML = e.target.value
 
 })
+
+
+
+// Grades average computing
+
+// Display average
+
+// Compute average
+
+// Retrieve grades
+function retrieveGrades(gradesID) {
+  let grades = []
+  for (const gradesIDElement of gradesID) {
+    grades.push(document.getElementById(gradesIDElement).value)
+  }
+  return grades
+}
+
+// Register listeners
+let inputs = document.getElementsByTagName('input')
+for (const input of inputs) {
+  input.addEventListener('input', function () {
+    console.log(retrieveGrades(['grade-1', 'grade-2']))
+  })
+}
